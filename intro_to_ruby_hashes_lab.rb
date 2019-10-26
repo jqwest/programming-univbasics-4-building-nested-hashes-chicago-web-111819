b66be72756cf9237b5ac4edda505a4d4e4b0ae63
john L <johnleavell@gmail.com>
2019-10-26 03:47:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	hash_without_the_keyword = {
-      :railroads => ""
+      :railroads => { }
   }
 end
 
