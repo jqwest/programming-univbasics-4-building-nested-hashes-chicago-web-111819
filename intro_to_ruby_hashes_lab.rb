06a52aa86c28b25822477bbc8f394d3ea82c3c74
john L <johnleavell@gmail.com>
2019-10-26 03:53:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,12 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  hash_without_the_keyword = {
+      :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => 100
+      }
+  }
 
 end
 
